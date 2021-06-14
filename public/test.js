8bf3d9056a5add7caf2279d1ab8f0d4d0d7393cb
JohansenJunias20 <johansen.gumbal@gmail.com>
2021-06-14 20:45:18 +0700

    removing node modules

--- public/test.js
+++
@@ -1,8 +0,0 @@
-
-canvas = document.getElementById("game-surface");
-/**  @type {WebGL2RenderingContext} */
-var gl = canvas.getContext('webgl2');
-console.log(gl)
-var vao = gl.createVertexArray();
-console.log("hello world")
-console.log(`vao : ${vao}`)
